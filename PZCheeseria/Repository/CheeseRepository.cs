48b1dd1422d0cd985fbaeeafb500d6dd0100d95b
MadadHanieh <103435652+MadadHanieh@users.noreply.github.com>
2022-04-12 00:18:49 +1000

    Add files via upload

---
+++ PZCheeseria/Repository/CheeseRepository.cs
@@ -0,0 +1,57 @@
+﻿using PZCheeseria.Model;
+using PZCheeseria.Repository.Interface;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PZCheeseria.Repository
+{
+    public class CheeseRepository : ICheeseRepository
+    {
+        private readonly IEnumerable<Cheese> AllCheese;
+        public CheeseRepository()
+        {
+            AllCheese = new List<Cheese>
+            {
+                 new Cheese
+                {
+                    Id = 1,
+                    Colour = "white and red",
+                    Price = 1
+                },
+                new Cheese
+                {
+                    Id = 2,
+                    Colour = "white and blue",
+                    Price = 2
+                },
+                new Cheese
+                {
+                    Id = 3,
+                    Colour = "white and green",
+                    Price = 3
+                },
+                new Cheese
+                {
+                    Id = 4,
+                    Colour = "white and purple",
+                    Price = 4
+                },
+                new Cheese
+                {
+                    Id = 5,
+                    Colour = "white and orange",
+                    Price = 5
+                }
+            };
+    }
+        public Cheese Get(int id)
+        {
+            return AllCheese.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Cheese> GetAllCheese()
+        {
+            return AllCheese;
+        }
+    }
+}
---
+++ PZCheeseria/Repository/Interface/ICheeseRepository.cs
@@ -0,0 +1,11 @@
+﻿using PZCheeseria.Model;
+using System.Collections.Generic;
+
+namespace PZCheeseria.Repository.Interface
+{
+    public interface ICheeseRepository
+    {
+        IEnumerable<Cheese> GetAllCheese();
+        Cheese Get(int id);
+    }
+}
